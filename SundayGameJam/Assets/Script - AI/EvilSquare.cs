02fd9f13d5a1b07980e31893ddf278cc3cb177fe
J.H.Hurst U1669330 <U1669330@unimail.hud.ac.uk>
2019-09-29 13:20:48 +0100

    Enemy Movement

---
+++ SundayGameJam/Assets/Script - AI/EvilSquare.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilSquare : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.AddForce(transform.right * speed, ForceMode2D.Impulse);
+
+        if(health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SundayGameJam/Assets/Script - AI/Meteor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.AddForce(transform.up * speed, ForceMode2D.Impulse);
+    }
+}
