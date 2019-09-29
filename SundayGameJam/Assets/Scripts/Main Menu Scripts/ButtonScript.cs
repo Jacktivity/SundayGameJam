10d940dc04df058c48454976023d4291841a0a80
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2019-09-29 14:52:15 +0100

    Bug fixes on main menu

--- SundayGameJam/Assets/Scripts/Main Menu Scripts/ButtonScript.cs
+++ SundayGameJam/Assets/Scripts/Main Menu Scripts/ButtonScript.cs
@@ -1,32 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-    Button startGameButton;
-    Button quitGameButton;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        startGameButton = GameObject.Find("Start Game Button").GetComponent<Button>();
-        quitGameButton = GameObject.Find("Quit Game Button").GetComponent<Button>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnClick() 
-    {
-
-    }
-
     public void StartGame() {
-        Debug.Log("Started");
+        SceneManager.LoadScene("Placeholder");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
     }
 }
